b0bfd1403a4938b39c10b6a1f1a5bfe588c32478
CheckYourScreen <nimitmehta95@gmail.com>
2016-10-27 10:16:46 +0200

    Add lcd notifier
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -21,6 +21,7 @@
 #include <linux/gpio.h>
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
+#include <linux/lcd_notify.h>
 
 #ifdef CONFIG_STATE_NOTIFIER
 #include <linux/state_notifier.h>
@@ -1057,6 +1058,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 
 	switch (event) {
 	case MDSS_EVENT_UNBLANK:
+		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
 		rc = mdss_dsi_on(pdata);
 		mdss_dsi_op_mode_config(pdata->panel_info.mipi.mode,
 							pdata);
@@ -1069,6 +1071,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_unblank(pdata);
 
 		state_suspended = false;
+		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 
 #ifdef CONFIG_STATE_NOTIFIER
 		if (!use_fb_notifier)
@@ -1076,6 +1079,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 #endif
 		break;
 	case MDSS_EVENT_BLANK:
+		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata);
 		break;
@@ -1086,6 +1090,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		rc = mdss_dsi_off(pdata);
 
 		state_suspended = true;
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 
 #ifdef CONFIG_STATE_NOTIFIER
 		if (!use_fb_notifier)
