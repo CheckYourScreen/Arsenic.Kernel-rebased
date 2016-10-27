4e965a0d572bd06b06332c9d0120b97ed95763bd
CheckYourScreen <nimitmehta95@gmail.com>
2016-10-27 15:56:24 +0200

    anykernel: import anykernel from aosp/cm version TODO: changes for oos version Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

---
+++ anykernel/ramdisk/sbin/arsenic-post_boot.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+############################
+# Custom Kernel Settings for Arsenic Kernel
+# Credits to RenderBroken & DerRomtester
+# Modified by Nimit Mehta
+echo "Boot Script Started" | tee /dev/kmsg
+
+# Dummy File for Future use (if any)
+
+
+echo "[Arsenic-Kernel] Boot Script Completed!" | tee /dev/kmsg
