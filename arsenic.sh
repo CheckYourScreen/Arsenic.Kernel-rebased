5e640885f66093db7f268529519da4e800f2faec
CheckYourScreen <nimitmehta95@gmail.com>
2017-05-05 20:31:54 +0200

    buildscript: change naming convention for rebased betas
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

--- arsenic.sh
+++ arsenic.sh
@@ -6,6 +6,8 @@
 BUILD_START=$(date +"%s")
 echo "enter version name for zip name (only number) :" 
 read VER
+echo "is it a test build ..? (y/n) :"
+read buildtype
 # Housekeeping
 KERNEL_DIR=$PWD
 cd ..
@@ -56,17 +58,15 @@ rm -rf $OUT_DIR/dtb
 cp $KERN_IMG $OUT_DIR/zImage
 # cp $KERN_DTB $OUT_DIR/dtb
 cd $OUT_DIR
-echo "is it a test build ..? (y/n) :"
-read buildtype
 case "$buildtype" in
 	y | Y)
 		echo "test build number?:"
 		read BN
-		zip -r -9 arsenic.kernel-onyx.R$VER-test-$BN.zip *
+		zip -r -9 arsenic.kernel-onyx.Rebased-beta$VER-test-$BN.zip *
 		echo "Test Build no. $BN of R$VER Ready..!"
 		;;
 	*)
-		zip -r -9 arsenic.kernel-onyx.R$VER-$(date +"%Y%m%d").zip *
+		zip -r -9 arsenic.kernel-onyx.Rebased-beta-$VER.zip *
 		echo "Release Build R$VER Ready..!!"
 		;;
 esac
